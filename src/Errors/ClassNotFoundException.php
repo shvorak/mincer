284b0ba7d7db98993c4432ab5707bd27546ff26d
Alexey Shvorak <dr.emerido@gmail.com>
2017-10-04 18:51:50 +0300

    Added missed exception

--- src/Errors/ClassNotFoundException.php
+++ src/Errors/ClassNotFoundException.php
@@ -1,15 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: emerido
- * Date: 04.10.2017
- * Time: 18:43
- */
 
-namespace Mincer\Errors;
+namespace Mincer\Errors {
 
+    class ClassNotFoundException extends \Exception
+    {
 
-class ClassNotFoundException
-{
+    }
 
 }
+
